7aa0dbe9070b70c233d7330039957e40572569f9
namirgon <namirgon@gmail.com>
2017-09-01 23:45:50 -0300

    primero de sep cambios CategoryController

---
+++ Presentation/ASF.UI.WbSite/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASF.UI.Process;
+using ASF.Entities;
+
+namespace ASF.UI.WbSite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var cp = new CategoryProcess();
+            return View(cp.SelectList());
+        }
+    }
+}
